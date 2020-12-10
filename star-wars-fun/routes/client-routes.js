0d99efe6e091910ccda36e9a6e426236b7665d87
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-09 19:39:56 -0800

    working now. wrong syntax in client-routes.

--- star-wars-fun/routes/client-routes.js
+++ star-wars-fun/routes/client-routes.js
@@ -1,24 +1,18 @@
 const router = require("express").Router();
 // path module
-const path = require("path")
+const path = require("path");
 
-// router.get("/characters", (req, res) => {
-//     fs.readFile("./data.json", "utf8", (err, data) => {
-//         if (err) throw err;
-//         console.log(data);
-//         res.json(JSON.parse(data));
-//     })
-// });
-router.get("/", (req, res) => {
-    // res.send(path.join(__dirname))
-    res.sendFile(path.join(__dirname), "../client/index.html")
-});
 
 router.get("/add", (req, res) => {
-    res.sendFile(path.join(__dirname),"../client/add.html")
+    res.sendFile(path.join(__dirname, "../client/add.html"));
 });
 
 router.get("/find", (req, res) => {
-    res.sendFile(path.join(__dirname),"../client/find.html")
+    res.sendFile(path.join(__dirname, "../client/find.html"));
 });
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
 module.exports = router;
--- star-wars-fun/server.js
+++ star-wars-fun/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const apiRoutes = require("./routes/api-routes");
 const clientRoutes = require("./routes/client-routes")
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/", clientRoutes)
+
 app.use("/api", apiRoutes)
-
+app.use("/", clientRoutes)
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
